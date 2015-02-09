bd00800c8e0ceac428ec19825601e9b79e97ebb3
Wes Modes <wmodes@gmail.com>
2015-02-09 15:51:03 -0800

    Added alpaca package and test form

--- meteor-deploy-instance.sh
+++ meteor-deploy-instance.sh
@@ -11,6 +11,9 @@ SSHSERVER="console.dc1.gpaas.net"
 SSHLOCATION="web/vhosts/default/"
 ARCH="os.linux.x86_64"
 AVAIL_NODE="0.10.36"
+README="README-DEPLOY.txt"
+
+ENV_VARS="MONGO_URL=mongodb://localhost:27017/secrethistory PORT=8080 ROOT_URL=http://localhost:8080"
 
 rm -rf ../$APPNAME.demeteorized/
 demeteorizer --app_name $APPNAME --output ../$APPNAME.demeteorized/
@@ -20,6 +23,24 @@ if [ "$?" != 0 ]; then
 fi
 cd ../$APPNAME.demeteorized
 perl -pi -e "s/\"node\": \"0\.10\..*\"/\"node\": \"$AVAIL_NODE\"/g" package.json
+perl -pi -e "s#\"start\": \"node .*\"#\"start\": \"$ENV_VARS node main.js\"#g" package.json
+cat > server.js <<EOT
+var forever = require('forever-monitor');
+
+var child = forever.start(['npm start'], {
+    'silent' : true
+});
+EOT
+cat > $README <<EOT
+After uploading to the PAAS instance, login to the instance and execute
+the following commands:
+
+cd $SSHLOCATION
+tar xvfz $APPNAME.tar.gz
+npm install
+
+Then reboot the instance.
+EOT
 tar cvfz ../$APPNAME.tar.gz .
 if [ "$?" != 0 ]; then 
     echo "Error: Tarball failed"
@@ -29,6 +50,7 @@ fi
 cd ..
 sftp $FTPUSER@$FTPSERVER <<EOT
 put $APPNAME.tar.gz $FTPLOCATION
+put $APPNAME/README-DEPLOY.txt $FTPLOCATION
 bye
 EOT
 if [ "$?" != 0 ]; then 
@@ -40,11 +62,12 @@ rm $APPNAME.tar.gz
 /bin/echo -n "Enable the console from the Gandi instance control panel: "; read; echo
 
 
-ssh $SSHUSER@$SSHSERVER <<EOT
+ssh -tt $SSHUSER@$SSHSERVER <<EOT
 
 
 cd $SSHLOCATION
 tar xvfz $APPNAME.tar.gz
+npm install
 EOT
 if [ "$?" != 0 ]; then 
     echo "Error: Deploy and restart"
@@ -61,6 +84,10 @@ https://www.gandi.net/admin/hosting/paas/89243
 
 cd $SSHLOCATION
 tar xvfz $APPNAME.tar.gz
+npm install
+
+5. Back at the Gandi page, reboot the instance.
+6. Test at http://$SERVER
 
 EOT
     exit 1
