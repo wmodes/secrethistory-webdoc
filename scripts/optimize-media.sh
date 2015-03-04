10ecb557b49afe4a37f756435bcae65a34f31bc2
Wes Modes <wmodes@gmail.com>
2015-03-03 23:18:44 -0800

    change for 480x272 to 720p encoding for video

--- scripts/optimize-media.sh
+++ scripts/optimize-media.sh
@@ -11,7 +11,8 @@ imgOpt=' -quality 50 -resize 1920x1920\> '
 #vidOpt=' -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k '
 
 handbrake='/usr/local/bin/HandbrakeCLI'
-vidOpt='-e x264 -q 22 -r 15 -B 64 -X 480 -O'
+#vidOpt='-e x264 -q 22 -r 15 -B 64 -X 480 -O' # 480x272
+vidOpt='-e x264 -q 22 -r 15 -B 64 -X 1280 -O'  # 1280X720
 
 echo "# Copying GIFs and PNGs"
 cd "$srcDir/$imgDir"
