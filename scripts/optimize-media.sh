c7b44a2c3c9535df5210530b1b73c1077393fcb0
Wes Modes <wmodes@gmail.com>
2015-03-03 23:10:37 -0800

    Create script to web optimize video and images

--- scripts/optimize-media.sh
+++ scripts/optimize-media.sh
@@ -7,8 +7,11 @@ audioDir="audio"
 
 imagemagick='/opt/local/bin/convert'
 imgOpt=' -quality 50 -resize 1920x1920\> '
-ffmpeg='/opt/local/bin/ffmpeg'
-vidOpt=' -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k '
+#ffmpeg='/opt/local/bin/ffmpeg'
+#vidOpt=' -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 -codec:a libfdk_aac -b:a 128k '
+
+handbrake='/usr/local/bin/HandbrakeCLI'
+vidOpt='-e x264 -q 22 -r 15 -B 64 -X 480 -O'
 
 echo "# Copying GIFs and PNGs"
 cd "$srcDir/$imgDir"
@@ -29,11 +32,14 @@ cd "$srcDir/$videoDir"
 for file in *.mp4
 do
     echo "  $file"
-    ffmpeg -i "$file" $vidOpt "$destDir/$file"
-    #"$ffmpeg" $file $imgOpt "$destDir/$imgDir/$file"
+    #echo ffmpeg -i \"$file\" $vidOpt \"$destDir/$file\"
+    #ffmpeg -i "$file" $vidOpt "$destDir/$videoDir/$file"
+    #echo $handbrake -i \"$file\" -o \"$destDir/$videoDir/$file\" $vidOpt
+    $handbrake -i "$file" -o "$destDir/$videoDir/$file" $vidOpt
 done
 
 echo "# Orig size:"
-du -sh "$srcDir/*"
+du -sh "$srcDir"/*/
 echo "# Final size:"
-du -sh "$destDir/*"
+du -sh "$destDir"/*/
+
