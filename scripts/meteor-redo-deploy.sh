e34356712316243288befc334796c3aa5a38a00c
Wes Modes <wmodes@gmail.com>
2015-03-30 00:34:16 -0700

    Attempts to deploy failed

---
+++ scripts/meteor-redo-deploy.sh
@@ -0,0 +1,25 @@
+
+STARTDIR="/Users/wmodes/dev/secrethistory/meteor"
+APPNAME="secrethistory"
+SERVER="doc.peoplesriverhistory.us"
+PORT="80"
+USER="wmodes"
+DEPLOY_LOCATION="/home/secrethistory"
+DEPLOY_USER="secrethistory"
+ARCH="os.linux.x86_64"
+
+ssh $USER@$SERVER <<EOT
+sudo cp -pr ~$USER/scripts/* $DEPLOY_LOCATION/scripts/
+sudo mv ~$USER/meteor.tar.gz $DEPLOY_LOCATION/
+cd $DEPLOY_LOCATION/
+echo > secrethistory.log
+# we are not deleting the existing bundle lest we nix the existing upload files
+#sudo rm -rf bundle
+sudo tar xvfz meteor.tar.gz
+cd bundle/programs/server/
+sudo npm install
+sudo chown -R $DEPLOY_USER.$DEPLOY_USER $DEPLOY_LOCATION
+sudo nginx -s reload
+sudo restart secrethistory
+sudo start secrethistory
+EOT
