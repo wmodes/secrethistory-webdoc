cdaba6613f955d5784f90ee48b32331e9f4e9b15
Wes Modes <wmodes@gmail.com>
2015-04-21 04:34:04 -0700

    Activated map element to opn map modal

---
+++ meteor/client/webdoc/navmenu.js
@@ -0,0 +1,12 @@
+
+// Secret History of American River People
+
+Template.navmenu.rendered = function() {
+
+  // Hide nav elements until revelaed
+  $('#mapburger').on("click", function() {
+    $('#navmenu').modal('show');
+  });
+
+
+}
