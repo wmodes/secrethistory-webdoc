95a240eff950a756dba4021ebe33963d8e12d58f
Wes Modes <wmodes@gmail.com>
2015-04-21 18:13:03 -0700

    commit before navmenu branch

--- meteor/client/webdoc/navmenu.js
+++ meteor/client/webdoc/navmenu.js
@@ -3,6 +3,19 @@
 
 Template.navmenu.rendered = function() {
 
+  // Load all chapters from collections
+  //
+  // Store some basic info about each
+  //   path, chapter, description, feature photo
+  //   toss out testing chapters
+  //   released chapters = ready
+  //   other chapters = future
+  //
+  // Sort into paths
+  //
+  // And sort by chapter number
+
+
   // Hide nav elements until revelaed
   $('#mapburger').on("click", function() {
     $('#navmenu').modal('show');
